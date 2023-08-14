255184930bad631236a41dfbf383f75372eb1893
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-14 23:29:49 +0530

    Topic-5-e

---
+++ springmvc/src/main/java/springmvc/controllers/MyController.java
@@ -0,0 +1,13 @@
+package springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
