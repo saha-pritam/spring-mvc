e90075deed93f5e696d8c1043a89820fcd093a1b
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-14 23:53:09 +0530

    Topic-6-d

--- springmvc/src/main/java/springmvc/controllers/MyController.java
+++ springmvc/src/main/java/springmvc/controllers/MyController.java
@@ -1,16 +1,15 @@
 package springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
 	
 	@RequestMapping("/home")
-	public String home(HttpServletRequest request) {
-		request.setAttribute("name", "Pritam");
+	public String home(Model model) {
+		model.addAttribute("name", "Aditya");
 		return "home";
 	}
 }
