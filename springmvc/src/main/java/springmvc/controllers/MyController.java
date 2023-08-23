2c5c00f6df0e149858b062a2b521b601fd9ac6d0
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-23 10:48:42 +0530

    Topic-8-g

--- springmvc/src/main/java/springmvc/controllers/MyController.java
+++ springmvc/src/main/java/springmvc/controllers/MyController.java
@@ -22,7 +22,13 @@ public class MyController {
 	
 	@ResponseBody
 	@RequestMapping(path = "/handleForm",method = RequestMethod.POST)
-	public String formController() {
+	public String formController(HttpServletRequest req) {
+		System.out.println("User Name :- "+req.getParameter("username"));
+		System.out.println("User Email :- "+req.getParameter("usermail"));
+		System.out.println("User Password :- "+req.getParameter("userpassword"));
+		System.out.println("User Occupation :- "+req.getParameter("useroccupation"));
+		System.out.println("News Letters :- "+req.getParameter("newsletters"));
+		System.out.println("Agree term :- "+req.getParameter("agreeterm"));
 		return "Form submitted";
 	}
 }
